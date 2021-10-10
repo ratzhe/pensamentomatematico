604500f52e9196a31031548c66ab409340c90761
ratzhe <81121205+ratzhe@users.noreply.github.com>
2021-10-10 19:50:03 -0300

    Delete lista2 directory

--- lista2/UsaContaCorrente.java
+++
@@ -1,9 +0,0 @@
-package br.pucpr.lista2;
-
-public class UsaContaCorrente {
-    public static void main(String[] args) {
-        ContaCorrente usuario = new ContaCorrente(1200);
-        usuario.sacar(800);
-        //usuario.depositar(800);
-    }
-}
